35eb20fe8488fd7e4b42d4fe70836ac96db1d847
Cwalker924 <cwalker924@gmail.com>
2017-03-23 23:53:21 +0000

    format changes

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Using puppet to fix type in .php file.
-exec { "sed":
-     path    => "/bin/",
-     command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
+exec { 'sed':
+  path    => '/bin/',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
 }
