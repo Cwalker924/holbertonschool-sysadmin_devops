9aaccc75a8c8875677d8764a774cf88792387994
Cwalker924 <cwalker924@gmail.com>
2017-04-18 04:17:34 +0000

    adding 2

---
+++ 0x17-api/2-export_to_JSON.py
@@ -0,0 +1,40 @@
+#!/usr/bin/pyhton3
+"""
+a Python script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress.
+
+Formart must be:
+
+{ "USER_ID": [ {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+"username": "USERNAME"}}, {"task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS, "username": "USERNAME"}}, ... ]}
+
+Then save to json file
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+        url = "https://jsonplaceholder.typicode.com/users"
+        empId = sys.argv[1]
+        user = requests.get("{}/{}"
+                            .format(url, empId)).json()
+        todos = requests.get("{}/{}/todos"
+                             .format(url, empId)).json()
+        uid = user.get("id")
+        uname = user.get("username")
+        utask = []
+        udict = {}
+
+        for todo in todos:
+                uinfo = {}
+                uinfo["task"] = todo.get("title")
+                uinfo["completed"] = todo.get("completed")
+                uinfo["username"] = uname
+                utask.append(uinfo)
+
+        udict[str(uid)] = utask
+        with open("{}.json".format(empId), mode="w", newline="") as tojson:
+                json.dump(udict, tojson)
