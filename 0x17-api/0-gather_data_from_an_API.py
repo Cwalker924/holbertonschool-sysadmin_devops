bcb3b8eebbd1d742379ddd97afedd6b48dee637f
Cwalker924 <cwalker924@gmail.com>
2017-04-18 02:44:59 +0000

    formating

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -9,24 +9,17 @@ import sys
 
 
 if __name__ == "__main__":
-        if len(sys.argv) > 1:
-            try:
-                url = "https://jsonplaceholder.typicode.com"
-                empId = sys.argv[1]
-                comp = []
-                if len(sys.argv) < 2:
-                    print("Need Employee ID")
-                else:
-                    name = requests.get("{}/users/{}"
-                                        .format(url, empId)).json().get("name")
-                    todo = requests.get("{}/todos?userId={}"
-                                        .format(url, empId)).json()
-                    for jobs in todo:
-                        if jobs.get("completed") is True:
-                            comp.append(jobs.get("title"))
-                    print("Employee {} is done with tasks({}/{}):"
-                          .format(name, len(comp), len(todo)))
-                    for task in comp:
-                        print("\t {}".format(task))
-            except (ValueError, TypeError):
-                pass
+        url = "https://jsonplaceholder.typicode.com"
+        empId = sys.argv[1]
+        comp = []
+        name = requests.get("{}/users/{}"
+                            .format(url, empId)).json().get("name")
+        todo = requests.get("{}/todos?userId={}"
+                            .format(url, empId)).json()
+        for jobs in todo:
+                if jobs.get("completed") is True:
+                        comp.append(jobs.get("title"))
+        print("Employee {} is done with tasks({}/{}):"
+              .format(name, len(comp), len(todo)))
+        for task in comp:
+                print("\t {}".format(task))
